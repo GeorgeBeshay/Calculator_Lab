523b3f63b5bf964b7353f6b502284603d28bce2f
George Beshay <georgebeshay101@gmail.com>
2022-11-29 18:19:40 +0200

    Delete CalculatorLab_BackEnd directory

--- CalculatorLab_BackEnd/mainPack/CalculatorBackServer/CalcFieldsI.java
+++
@@ -1,4 +0,0 @@
-package mainPack.CalculatorBackServer;
-
-public interface CalcFieldsI {
-}
--- CalculatorLab_BackEnd/mainPack/CalculatorBackServer/ISolver.java
+++
@@ -1,5 +0,0 @@
-package mainPack.CalculatorBackServer;
-
-public interface ISolver {
-	public CalcFields solve(CalcFields calcFields);
-}
